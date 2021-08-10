7d5295057ef108e5d43b5267f567845668961cc2
Richard King <richard@renewableenergyhub.com.au>
2021-08-10 18:56:19 +1000

    select members marked new and check all of them

--- cypress/integration/500 email/500 email.js
+++ cypress/integration/500 email/500 email.js
@@ -3,8 +3,8 @@ import { And, Given, Then, When } from "cypress-cucumber-preprocessor/steps";
 
 // to become gen filter test
 Then('I select a member', (str) => {
-    cy.get("#searchBox").focus().clear().type("Richard")
-    cy.get('.checkrow:visible').click()
+    cy.get("#searchBox").focus().clear().type("New")
+    cy.get('.checkrow:visible').click({"multiple":true})
 })
 
 
