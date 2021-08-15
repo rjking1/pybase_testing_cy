86f1f13db6c95b16ba549fa4cb35ea84fe493a3c
Richard King <richard@analytics.com.au>
2021-08-15 11:19:18 +1000

    inc restore timeout; tidy commands

--- cypress/integration/common/bu restore.js
+++ cypress/integration/common/bu restore.js
@@ -7,12 +7,12 @@ And("Backup the {string} db", (db) => {
   cy.get("#bu_db").focus().clear().type(db);
   cy.get("#bu_file").focus().clear().type(BU_FILE_NAME);
   cy.get("#backup").click();
-  cy.contains("Backed up", { timeout: 10000 });
+  cy.contains("Backed up", { timeout: 20000 });
 });
 
 And("Restore to the test db", () => {
   cy.get("#rest_file").focus().clear().type(BU_FILE_NAME);
   cy.get("#rest_db").focus().clear().type(TEST_DB); // should be set to test but belt and braces
   cy.get("#restore").click();
-  cy.contains('Restored', { timeout: 10000 })
+  cy.contains("Restored", { timeout: 20000 });
 });
